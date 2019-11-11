0ee3756f62ea1a0f62d1f0e76069eda5abcd036c
Luan Figueredo <luanrubensf@gmail.com>
2019-11-11 13:32:29 -0300

    Adicionando docker

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # node-docker
 Node project working with docker
 
-
 npm init -y
 
 npm install express
@@ -16,8 +15,71 @@ npm install redis
 
 Integrar com redis
 
+npm i --save body-parser
+
+npm i uuid
+
+================== node ==================
+
+node --version
+node: operar no console
+
+================== docker ==================
+
+docker version
+
+docker run hello-world
+
+docker run busybox echo hello world
+
+================== Construindo a imagem de dev ==================
+
+docker build -f Dockerfile.dev -t node-docker-v1 .
+
+docker run -p 3000:3000 -v /app/node_modules -v %cd%:/app node-docker-v1
+
+docker run -p 3000:3000 -v /app/node_modules -v $(pwd):/app node-docker-v1
+
 ================== Rodar redis  ==================
 
-docker run -d redis
+docker run -d redis -- sem mapeamento, não será possível acessar local
 
 docker run -d -p 6379:6379 redis
+
+================== GIT  ==================
+
+git init
+
+git status
+
+criar arquivo
+
+git add teste.txt
+
+git commit -m "arquivo teste.txt"
+
+git status
+
+git remote add origin https://github.com/luanrubensf/teste-git.git
+
+git push -u origin master
+
+git branch
+
+git branch teste
+
+git checkout teste
+
+alterar arquivo branch e master e commitar
+
+trocar para master
+
+git merge teste
+
+git status
+
+ajustar arquivo
+
+git commit -m "merge teste"
+
+============================================
--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 const express = require('express');
 const redis = require('redis');
+const bodyParser = require('body-parser');
+const uuidV4 = require('uuid/v4');
+const async = require('async');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.use(bodyParser.json());
 
 const redisClient = redis.createClient({
-    host: 'localhost',
+    host: 'redis-server', //ou localhost ou 'redis-server'
     port: 6379
 });
 
-redisClient.set('visits', 0);
-
 app.get('/', (req, res) => {
-    redisClient.get('visits', (err, visits) => {
-        res.send('Number of visits is: ' + visits);
-        redisClient.set('visits', parseInt(visits) + 1);
+    redisClient.keys('*', function (err, keys) {
+        if (err) { 
+            return console.log(err);
+        }
+        if(keys){
+            async.map(keys, function(key, cb) {
+                redisClient.get(key, function (error, value) {
+                    if (error) { 
+                        return cb(error);
+                    }
+                    cb(null, JSON.parse(value));
+                }); 
+            }, function (error, results) {
+               if (error) {
+                   return console.log(error);
+               }
+               console.log(results);
+               res.json({data:results});
+            });
+        }
     });
+});
+
+app.post('/', (req, res) => {
+    const body = req.body;
+    body.id = uuidV4();
+    redisClient.set(body.id, JSON.stringify(body));
+    res.send(body);    
 });
 
 app.listen(PORT, HOST, () => {
